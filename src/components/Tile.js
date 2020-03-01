28cc183ea4406e3d6bc69a255cb737d97f20247b
kristian woods <kris_woods@hotmail.co.uk>
2020-03-01 18:17:04 +0000

    add arrow down to tile

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -6,7 +6,6 @@ class Tile extends Component {
     state = { 
         tiles: data
      }
-
      handleDropDown = id => {
         this.setState(prevState => ({
           tiles: prevState.tiles.map((tile, i) => ({
@@ -17,17 +16,17 @@ class Tile extends Component {
       };
     
     render() {
-
-        console.log(this.state.tiles);
         let tile = this.state.tiles.map((item, index) => (
             <div 
             className={"tile-section " + (item.isActive ? "activeTile" : "")}
             key={index} 
             onClick={()=>{this.handleDropDown(index)}} 
             >
-            <div className="tile" style={{backgroundImage: `url(${item.image})`}}>
-                    <h3>{item.title}</h3>
-                    <div className="tile__hover-overlay"></div>   
+                <div className="tile" style={{backgroundImage: `url(${item.image})`}}>
+                    <div className="tile__hover-overlay">
+                        <h3>{item.title}</h3>
+                    </div> 
+                    <div class="arrow-down"></div>  
                 </div>
                 <TileDropDown body={item.body} image={item.image}/> 
             </div>
--- src/components/TileDropDown.js
+++ src/components/TileDropDown.js
@@ -4,7 +4,7 @@ const TileDropDown = (props) => {
     return ( 
         <div className="tile__dropdown">
             <p>{props.body}</p>
-            <img src={props.image} alt="test"/>
+            <img src={props.image} alt={props.image}/>
         </div>
      );
 }
