f8ec21a42d96ba6e306dd8fac0365a35dae74248
kristian woods <kris_woods@hotmail.co.uk>
2020-03-11 11:08:39 +0000

    move event handler to tile instead of containing el

--- src/components/TileComponent/Tile.js
+++ src/components/TileComponent/Tile.js
@@ -7,8 +7,8 @@ import PropTypes from 'prop-types';
 const Tile = ({item, index, handleDropDown, handleDropDownKeyPress}) => {
     return ( 
 
-        <div className={"tile-section " + (item.isActive ? "tile-section--active-tile" : "")} tabIndex="0" aria-label={(item.isActive) ? (`${item.title} is open`) : (`${item.title} is closed`) } aria-live="assertive" onKeyPress={(e) => {handleDropDownKeyPress(index, e)}} onClick={()=>{handleDropDown(index)}}>
-                <TileThumbnail title={item.title} image={item.image}/>
+        <div className={"tile-section " + (item.isActive ? "tile-section--active-tile" : "")} aria-label={(item.isActive) ? (`${item.title} is open`) : (`${item.title} is closed`) } aria-live="assertive">
+                <TileThumbnail title={item.title} image={item.image} handleDropDown={handleDropDown} handleDropDownKeyPress={handleDropDownKeyPress} index={index}/>
                 <TileDropDown title={item.title} body={item.body} image={item.image} cta={item.cta} /> 
         </div>
 
--- src/components/TileComponent/TileThumbnail.js
+++ src/components/TileComponent/TileThumbnail.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 
 
-const TileThumbnail = ({image, title}) => {
+const TileThumbnail = ({image, title, index, handleDropDownKeyPress, handleDropDown}) => {
     return ( 
-        <div className="tile" style={{backgroundImage: `url(${image.src})`}}>
+        <div className="tile" style={{backgroundImage: `url(${image.src})`}}tabIndex="0" onKeyPress={(e) => {handleDropDownKeyPress(index, e)}} onClick={()=>{handleDropDown(index)}}>
             <div className="tile__hover-overlay">
                 <h3>{title}</h3>
             </div> 
@@ -17,6 +17,9 @@ const TileThumbnail = ({image, title}) => {
 TileThumbnail.propTypes = {
     image: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
+    index: PropTypes.number.isRequired,
+    handleDropDownKeyPress: PropTypes.func.isRequired,
+    handleDropDown: PropTypes.func.isRequired
   }
  
 export default TileThumbnail;
